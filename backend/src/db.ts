9345a19009e97eb2fcf29ef32018520ea9120e19
Braeden Treutel <braeden4328@gmail.com>
2025-07-22 00:35:47 -0500

    Testing

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -9,6 +9,10 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false // Required for Render-hosted DBs
+  }
 });
 
 export default pool;
